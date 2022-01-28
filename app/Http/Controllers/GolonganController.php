a8e2def153d8c74dceda8a633de8853fda07b354
oong26 <mkhalil26122000@gmail.com>
2022-01-28 23:16:53 +0700

    Master golongan.

---
+++ app/Http/Controllers/GolonganController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GolonganRequest;
+use App\Models\Golongan;
+use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Database\QueryException;
+
+class GolonganController extends Controller
+{
+    private $param;
+
+    public function __construct()
+    {
+        $this->param['pageTitle'] = 'Golongan';
+        $this->param['pageIcon'] = 'feather icon-map-pin';
+        $this->param['parentMenu'] = '/golongan';
+        $this->param['current'] = 'Golongan';
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $this->param['btnText'] = 'Tambah';
+        $this->param['btnLink'] = route('golongan.create');
+
+        try {
+            $keyword = $request->get('keyword');
+            $getGolongan = Golongan::orderBy('id');
+
+            if ($keyword) {
+                $getGolongan->where('golongan', 'LIKE', "%{$keyword}%");
+            }
+
+            $this->param['data'] = $getGolongan->paginate(10);
+        } catch (\Illuminate\Database\QueryException $e) {
+            return back()->withError('Terjadi Kesalahan : ' . $e->getMessage());
+        }
+        catch (Exception $e) {
+            return back()->withError('Terjadi Kesalahan : ' . $e->getMessage());
+        }
+
+        return \view('golongan.index', $this->param);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $this->param['btnText'] = 'List golongan';
+        $this->param['btnLink'] = route('golongan.index');
+
+        return \view('golongan.create', $this->param);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GolonganRequest $request)
+    {
+        $validated = $request->validated();
+        try {
+            $golongan = new Golongan;
+            $golongan->pangkat = $validated['pangkat'];
+            $golongan->save();
+        } catch (Exception $e) {
+            return back()->withError('Terjadi kesalahan.');
+        } catch (QueryException $e) {
+            return back()->withError('Terjadi kesalahan pada database.');
+        }
+
+        return redirect()->route('golongan.index')->withStatus('Data berhasil disimpan.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $this->param['data'] = Golongan::find($id);
+        $this->param['btnText'] = 'List Golongan';
+        $this->param['btnLink'] = route('golongan.index');
+
+        return view('golongan.edit', $this->param);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(GolonganRequest $request, $id)
+    {
+        $golongan = Golongan::findOrFail($id);
+
+        $validated = $request->validated();
+
+        try {
+            $golongan->pangkat = $validated['pangkat'];
+
+            $golongan->save();
+        } catch (\Exception $e) {
+            return redirect()->back()->withError('Terjadi kesalahan.');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database.');
+        }
+
+        return redirect()->route('golongan.index')->withStatus('Data berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $golongan = Golongan::findOrFail($id);
+            $golongan->delete();
+        } catch (Exception $e) {
+            return back()->withError('Terjadi kesalahan.');
+        } catch (QueryException $e) {
+            return back()->withError('Terjadi kesalahan pada database.');
+        }
+
+        return redirect()->route('golongan.index')->withStatus('Data berhasil dihapus.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GolonganController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+
+    Route::resource('golongan', GolonganController::class);
+});
 
 require __DIR__.'/auth.php';
